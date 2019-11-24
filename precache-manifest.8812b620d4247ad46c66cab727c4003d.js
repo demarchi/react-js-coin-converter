ed7ed64d95f4213a84f8b3ecfe4c0b52ea11823d
Bruno de Marchi <bruno1549@gmail.com>
2019-11-24 17:36:23 +0000

    Updates

---
+++ precache-manifest.8812b620d4247ad46c66cab727c4003d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a869cdcf2b1d875e6b28e42c2d9f5208",
+    "url": "/react-js-coin-converter/index.html"
+  },
+  {
+    "revision": "4162a7c8cdef2519d070",
+    "url": "/react-js-coin-converter/static/css/main.e9d1c195.chunk.css"
+  },
+  {
+    "revision": "d4e49609fe9b0e557fb5",
+    "url": "/react-js-coin-converter/static/js/2.ed22e86f.chunk.js"
+  },
+  {
+    "revision": "4162a7c8cdef2519d070",
+    "url": "/react-js-coin-converter/static/js/main.f9df3b16.chunk.js"
+  },
+  {
+    "revision": "bd816be9be3526641d46",
+    "url": "/react-js-coin-converter/static/js/runtime-main.551d5947.js"
+  }
+]);
