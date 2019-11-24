c2c9f66282d94b0eab565dda08f03c069623d694
Bruno de Marchi <bruno1549@gmail.com>
2019-11-24 17:19:07 +0000

    Added React project files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+import Conversor from "./components/converter"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Coin Converter</h1>
+      <div className="row">
+        <Conversor coinA="GBP" coinB="BRL"></Conversor>
+        <Conversor coinA="BRL" coinB="GBP"></Conversor>
+      </div>
+      <div className="row">
+        <Conversor coinA="USD" coinB="BRL"></Conversor>
+        <Conversor coinA="BRL" coinB="USD"></Conversor>
+      </div>
+      <div className="row">
+        <Conversor coinA="EUR" coinB="BRL"></Conversor>
+        <Conversor coinA="BRL" coinB="EUR"></Conversor>
+      </div>
     </div>
   );
 }
